6b4c3327e5faa799558acb179ad73ef08c40411a
Nitinraj Arvind <nitinarv@gmail.com>
2016-10-24 17:12:20 +0530

    Adding google indexing code into this sample

---
+++ app/src/main/java/com/sgxp/googleindexing/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sgxp.googleindexing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.group.studyproject.R;
+
+/**
+ * Created by narvind on 24/10/16.
+ */
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_indexing_main);
+    }
+}
